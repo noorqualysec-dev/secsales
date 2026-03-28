438cf2b2b37c358de947d5f46d8ddb8057f8c44e
Noor Alam <noor.qualysec@gmail.com>
2026-03-29 01:08:01 +0530

    refactor : nextConfig.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
+  async headers() {
     return [
       {
         // Any request to /api/:path* from the frontend
         // gets transparently proxied to the backend — no CORS!
         source: "/api/:path*",
-        destination: "https://secsales-backend-6rd9f2le1-noorqualysec-1339s-projects.vercel.app/api/:path*",
+        // destination: "https://secsales-backend-6rd9f2le1-noorqualysec-1339s-projects.vercel.app/api/:path*",
+         headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
       },
+      // {
+      //   source: "/api/:path*",
+      //   headers: [
+      //     { key: "Access-Control-Allow-Origin", value: "*" },
+      //     { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+      //     { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+      //   ],
+      // },
     ];
   },
 };
